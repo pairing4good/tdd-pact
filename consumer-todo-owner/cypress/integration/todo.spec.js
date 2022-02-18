bc65afeda687b2dbe8f8153bbc8e7d2275f95e7e
David <pairing4good@gmail.com>
2022-02-18 16:05:54 -0600

    test drove tasks grouped by owner

---
+++ consumer-todo-owner/cypress/integration/todo.spec.js
@@ -0,0 +1,35 @@
+describe('List ToDos', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should list all todos', () => {
+    cy.get('.todo-list li span.todo-owner').should('have.length', 5)
+
+    cy.get('.todo-list li span.todo-owner').eq(0).should('have.text', 'bsmith')
+    cy.get('.todo-list-0 li').should('have.length', 3)
+    cy.get('.todo-list-0 li').eq(0).should('have.text', 'Take out the garbage')
+    cy.get('.todo-list-0 li').eq(1).should('have.text', 'Feed the dog')
+    cy.get('.todo-list-0 li').eq(2).should('have.text', 'File taxes')
+
+    cy.get('.todo-list li span.todo-owner').eq(1).should('have.text', 'hjones')
+    cy.get('.todo-list-1 li').should('have.length', 2)
+    cy.get('.todo-list-1 li').eq(0).should('have.text', 'Wash the dishes')
+    cy.get('.todo-list-1 li').eq(1).should('have.text', 'Winterize the house')
+
+    cy.get('.todo-list li span.todo-owner').eq(2).should('have.text', 'zloda')
+    cy.get('.todo-list-2 li').should('have.length', 1)
+    cy.get('.todo-list-2 li').eq(0).should('have.text', 'Fold the laundry')
+
+    cy.get('.todo-list li span.todo-owner').eq(3).should('have.text', 'mwhite')
+    cy.get('.todo-list-3 li').should('have.length', 2)
+    cy.get('.todo-list-3 li').eq(0).should('have.text', 'Weed the garden')
+    cy.get('.todo-list-3 li').eq(1).should('have.text', 'Renew your drivers license')
+
+    cy.get('.todo-list li span.todo-owner').eq(4).should('have.text', 'jbrown')
+    cy.get('.todo-list-4 li').should('have.length', 2)
+    cy.get('.todo-list-4 li').eq(0).should('have.text', 'Change the car oil')
+    cy.get('.todo-list-4 li').eq(1).should('have.text', 'Fix the gate')
+  })
+
+})
---
+++ consumer-todo-owner/src/ToDoRepository.js
@@ -0,0 +1,37 @@
+export function findAll(){
+    return [ 
+      {
+        "owner":"bsmith",
+        "todos":[
+          {"id":1,"description":"Take out the garbage"},
+          {"id":4,"description":"Feed the dog"},
+          {"id":8,"description":"File taxes"}
+        ]
+      },
+      {
+        "owner":"hjones",
+        "todos":[
+          {"id":2,"description":"Wash the dishes"},
+          {"id":5,"description":"Winterize the house"}
+        ]
+      },
+      {
+        "owner":"zloda",
+        "todos":[{"id":3,"description":"Fold the laundry"}]
+      },
+      {
+        "owner":"mwhite",
+        "todos":[
+          {"id":6,"description":"Weed the garden"},
+          {"id":19,"description":"Renew your drivers license"}
+        ]
+      },
+      {
+        "owner":"jbrown",
+        "todos":[
+          {"id":7,"description":"Change the car oil"},
+          {"id":9,"description":"Fix the gate"}
+        ]
+      }
+    ]
+};
